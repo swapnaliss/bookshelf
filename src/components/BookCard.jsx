e9f0a0e53f6e5c22d6c9e87180e94700771df6a9
swapnaliss <swapnalisbhargude123@gmail.com>
2023-06-17 22:19:01 +0530

    added functionality

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 import { Card, Dropdown } from 'react-bootstrap';
 
 const BookCard = ({ book, updateBooksStatus }) => {
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Card, Button, Row } from 'react-bootstrap';
 import BookCard from './BookCard';
 
 const Search = () => {
