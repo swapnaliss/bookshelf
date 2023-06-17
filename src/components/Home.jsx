07b000673e77446667dacfa1795244654141b20a
swapnaliss <swapnalisbhargude123@gmail.com>
2023-06-17 20:02:08 +0530

    UI basic setup complete

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bookshelf - Home</h1>
+      <Link to="/search">Search</Link>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Search = () => {
+  return (
+    <div>
+      <h1>Bookshelf - Search</h1>
+      <Link to="/">Back to Home</Link>
+    </div>
+  );
+};
+
+export default Search;
